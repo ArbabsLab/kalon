03a4ecd7c06201d351cb912ab28bcae1de8c69a3
Arbab <arbabhusain200@gmail.com>
2025-10-17 21:52:38 -0400

    Added routes and handlers for auth

---
+++ backend/handlers/authHandlers.js
@@ -0,0 +1,12 @@
+export const signup = async (req, res) => {
+    res.send("hi")
+}
+
+export const signin = async (req, res) => {
+    res.send("hi")
+}
+
+export const signout = async (req, res) => {
+    res.send("hi")
+}
+
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import authRoutes from "./routes/authRoutes.js"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -8,6 +8,7 @@ if (!PORT){
     console.log("PORT number not specified")
 }
 
+app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
     console.log(`${PORT}`)
 })
