e6aada4aafc52ddb9b08605b3927b20d9ea7829c
Arbab <arbabhusain200@gmail.com>
2025-10-19 15:03:49 -0400

    Added signup route logic

--- backend/handlers/authHandlers.js
+++ backend/handlers/authHandlers.js
@@ -1,4 +1,37 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/userModels.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+    const access_token = jwt.sign({userId}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    })
+
+    const refresh_token = jwt.sign({userId}, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d",
+    })
+
+    return {access_token, refresh_token};
+}
+
+const storeRefreshToken = async(userId, refreshToken) => {
+    await redis.set(`${userId}`, refreshToken, "EX", 7*24*60*60)
+}
+
+const setCookies = (res, accessToken, refreshToken) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 15*60*1000,
+    })
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 7*24*60*60*1000,
+    })
+}
+
 
 export const signup = async (req, res) => {
     const {email, password, name} = req.body;
@@ -10,8 +43,15 @@ export const signup = async (req, res) => {
 
     const user = await User.create({name, email, password});
 
-    res.status(201).json({user, message: "User created"});
-    
+    const {access_token, refresh_token} = generateToken(user._id);
+    await storeRefreshToken(user._id, refresh_token);
+
+    setCookies(res, access_token, refresh_token)
+    res.status(201).json({user: {
+        _id: user._id,
+        name: user.name,
+    }, message: "User created"});
+
     } catch (e){
         res.status(500).json({message: e.message})
     }
