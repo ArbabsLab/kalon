37d0e81a8e7c7f955ac59fd2c0b20c4fbc38b84f
Arbab <arbabhusain200@gmail.com>
2025-10-21 23:31:34 -0400

    Added featured products routes

--- backend/handlers/productHandlers.js
+++ backend/handlers/productHandlers.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/productModels.js"
 export const getProducts = async (req, res) => {
     try{
@@ -7,4 +8,23 @@ export const getProducts = async (req, res) => {
         res.status(500).json({message: e.message})
     }
     
+}
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featured_products = await redis.get("featured_products");
+        if (featured_products){
+        return res.json(JSON.parse(featured_products))
+        }
+
+        featured_products = await Product.find({isFeatured: true}).lean();
+        if (!featured_products){
+            return res.status(400).json({message: "No featured products"});
+        }
+        await redis.set("featured_products", JSON.stringify(featured_products));
+        res.json(featured_products);
+
+    } catch (e){
+
+    }
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProducts } from "../handlers/productHandlers.js";
+import { getProducts, getFeaturedProducts } from "../handlers/productHandlers.js";
 import { protectRoute, adminRoute } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 router.get("/", protectRoute, adminRoute, getProducts);
+router.get("/featured", getFeaturedProducts);
 
 export default router;
