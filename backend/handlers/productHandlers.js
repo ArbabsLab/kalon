bbd345faf47330c9e7c415ce4f51130fed4b94f1
Arbab <arbabhusain200@gmail.com>
2025-10-23 21:54:39 -0400

    Added creating product handler

--- backend/handlers/productHandlers.js
+++ backend/handlers/productHandlers.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { redis } from "../lib/redis.js";
 import Product from "../models/productModels.js"
 export const getProducts = async (req, res) => {
@@ -25,6 +26,26 @@ export const getFeaturedProducts = async (req, res) => {
         res.json(featured_products);
 
     } catch (e){
+        res.status(500).json({message: e.message})
+    }
+}
 
+export const createProducts = async (req, res) => {
+    try{
+        const {name, description, price, image, category} = req.body;
+
+        let cldnry = null;
+
+        if (image){
+            cldnry = await cloudinary.uploader.upload(image, {folder: "products"} );
+        }
+
+        const product = await Product.create({
+            name, description, price, image: cldnry.secure_url, category
+        });
+
+        res.status(200).json(product);
+    } catch (e) {
+        res.status(500).json({message: e.message})
     }
 }
